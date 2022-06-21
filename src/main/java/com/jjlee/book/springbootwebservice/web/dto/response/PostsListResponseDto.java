362eadb978aa50c36fa9f4b5d25ce9f67f8abf9c
LeeJejune <leejejune0625@gmail.com>
2022-06-21 17:55:32 +0900

    add: 4장 실습

---
+++ src/main/java/com/jjlee/book/springbootwebservice/web/dto/response/PostsListResponseDto.java
@@ -0,0 +1,22 @@
+package com.jjlee.book.springbootwebservice.web.dto.response;
+
+import com.jjlee.book.springbootwebservice.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
+
