cd4257d2b820f0ae2aa05f0d5c62378b5e6a9b3d
LeeJejune <leejejune0625@gmail.com>
2022-06-22 15:10:34 +0900

    feat: 세션 관리 어노테이션으로 변경

--- src/main/java/com/jjlee/book/springbootwebservice/web/IndexController.java
+++ src/main/java/com/jjlee/book/springbootwebservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jjlee.book.springbootwebservice.web;
 
+import com.jjlee.book.springbootwebservice.config.auth.LoginUser;
 import com.jjlee.book.springbootwebservice.config.auth.dto.SessionUser;
 import com.jjlee.book.springbootwebservice.service.posts.PostsService;
 import com.jjlee.book.springbootwebservice.web.dto.response.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null){
             model.addAttribute("userName", user.getName());
         }
