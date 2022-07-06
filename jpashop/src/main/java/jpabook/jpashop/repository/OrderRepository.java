4925077c2f640dbc7921901e8cf6fbf145d941c8
LeeJejune <leejejune0625@gmail.com>
2022-07-06 16:18:52 +0900

    feat: orderRepository 추가

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -3,9 +3,7 @@ package jpabook.jpashop.repository;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
@@ -60,5 +58,47 @@ public class OrderRepository {
         cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
         TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //최대 1000건
         return query.getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class
+        ).getResultList();
+    }
+
+    public List<OrderSimpleQueryDto> findOrderDtos() {
+        // 보통 이런 쿼리들은 order.simpleQuery 패키지를 나눠 사용. 리포지토리를 따로 만든다.
+        // OrderRepository -> 순수한 엔티티로 조회.
+        return em.createQuery(
+                        "select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name," +
+                                "o.orderDate, o.status, d.address) " +
+                                " from Order o" +
+                                " join o.member m" +
+                                " join o.delivery d",
+                        OrderSimpleQueryDto.class)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithItem() {
+        //1대N을 패치조인 하는 순간 페이징 불가.
+        return em.createQuery(
+                "select distinct o from Order o " +
+                        "join fetch o.member m" +
+                        " join fetch o.delivery d " +
+                        " join fetch o.orderItems oi " +
+                        "join fetch oi.item i", Order.class
+        ).getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select distinct o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class
+        )       .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
     }
 }
